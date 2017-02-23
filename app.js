d066e2e8bdcef811cf65dcbbe820a93b73af443f
ulyanatsyukh <ut@delte.com>
2017-02-23 13:29:33 +0100

    new chat

--- app.js
+++ app.js
@@ -1,53 +1,16 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-
-//=========================================================
-// Bot Setup
-//=========================================================
-
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res){
+ res.sendFile(__dirname + '/index.html');
 });
-  
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+io.on('connection', function(socket){
+ socket.on('chat message', function(msg){
+ io.emit('chat message', msg);
+ });
 });
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+http.listen(port, function(){
+ console.log('listening on ' + port);
 });
-
-function getJSONDataFromUrl(){
-    var query = "?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo";
-    var options = {};
-    options.url = "http://api.geonames.org";
-    options.type = options.type || "json";
-    options.path = "/citiesJSON" + query;
-    options.headers = {Accept: "application/json"};
-
-
-    var client = restify.createClient(options);
-
-    client.get(options, function(err, req, res, data) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        client.close();
-        console.log(JSON.stringify(data));
-
-        return JSON.stringify(data);
-    });
-}
-
-getJSONDataFromUrl();
