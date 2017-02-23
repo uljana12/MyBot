3267b9bfb4b9faba4606d6582bed10ee518e40f6
ulyanatsyukh <ut@delte.com>
2017-02-23 13:51:20 +0100

    new chat version

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3987, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- io.emit('chat message', msg);
- });
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 3721f2a3-d448-404e-8645-5f7436b61b09,
+    appPassword: bXj09MpPMcnQJEo8JSPnu3j
 });
-http.listen(port, function(){
- console.log('listening on ' + port);
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
 });
